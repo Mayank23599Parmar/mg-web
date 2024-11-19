c635457b272c6c7ff13d326d2a24414ac044fa8f
mayank23599 <mayankparmar23599@gmail.com>
2024-11-20 01:17:28 +0530

    deploy

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -2,7 +2,7 @@
 const FloatingButton = () => {
   return (
     <button
-      className="fixed bottom-4 right-4 bg-[hsl(274deg_37.82%_53.33%)] text-white font-medium py-3 px-6 rounded-full shadow-lg hover:opacity-90 transition-opacity md:bottom-6 md:right-6"
+      className="cursor-pointer fixed bottom-4 right-4 bg-[hsl(274deg_37.82%_53.33%)] text-white font-medium py-3 px-6 rounded-full shadow-lg hover:opacity-90 transition-opacity md:bottom-6 md:right-6"
       onClick={() => window.location.href='https://apps.shopify.com/mg-pwa-pro'} // Replace with your functionality
     >
       Install Our App

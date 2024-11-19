71abd1de23eba06d6c3bf93e05d285cc37af5c6d
mayank23599 <mayankparmar23599@gmail.com>
2024-11-20 01:07:01 +0530

    deploy"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import HeroBanner from "./HeroBanner";
 
 export default function Home() {
   return (
-    <div className="home-page">
+    <div className="home-page pt-20">
           <HeroBanner url="https://cdn.shopify.com/s/files/1/0649/9912/9262/files/MG-07.jpg?v=1732043814" />
       <HeroBanner url="https://cdn.shopify.com/s/files/1/0649/9912/9262/files/MG-04.jpg?v=1732043814" />
       <HeroBanner url="https://cdn.shopify.com/s/files/1/0649/9912/9262/files/MG-05.jpg?v=1732043814" />
